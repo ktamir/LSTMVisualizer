271d55444ae0a2a816ed9e92f5a76f07f42ec42a
Gilad Bison <gilad@appdome.com>
2019-01-10 18:11:53 +0200

    implement login and signup

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: { type: String, required: true },
+    email: { type: String, required: true, index: {unique: true, dropDups: true} },
     password: { type: String, required: true },
     created_at: Date
 });
--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const boom = require('boom');
+const _ = require('lodash');
+
 const mongo = require('./lib/mongo');
 const userModel = require('./models/user');
 
@@ -8,24 +12,50 @@ const port = 3000;
 
 mongo.init();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/createUser', async (req, res) => {
+app.post('/signup', async (req, res, next) => {
+    console.log('signup called');
+
+    const dbUsers = await userModel.find({email: req.body.email});
+
+    if (!_.isEmpty(dbUsers)) {
+        return next(boom.badRequest(`There is already a user with the email ${req.body.email}`));
+    }
+
     const newUser = userModel({
-        email: 'lala2@lala.com',
-        password: 'lala123',
+        email: req.body.email,
+        password: req.body.password,
         created_at: new Date()
     });
 
-    const result = await newUser.save();
-    console.log(result);
-
-    res.send('CREATED');
+    try {
+        const createdUser = await newUser.save();
+        res.send({user_token: createdUser._id});
+    }
+    catch(e) {
+        return next(boom.internal(e));
+    }
 });
 
-app.post('/signup', (req, res) => res.send('Signup - create user and return user token'));
+app.post('/login', async (req, res, next) => {
+    console.log('login called');
+    const dbUser = await userModel.find({email: req.body.email});
 
-app.post('/login', (req, res) => res.send('return user token'));
+    if (_.isEmpty(dbUser)) {
+        return next(boom.notFound(`No user with email ${req.body.email} found in database`));
+    }
+
+    if (_.get(dbUser, '[0].password') !== req.body.password) {
+        return next(boom.forbidden('Bad Password'));
+    }
+
+    res.send({user_token: dbUser[0]._id});
+});
 
 app.post('/projects', (req, res) => res.send('Add new project'));
 
