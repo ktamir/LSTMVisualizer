38119189a8893e87d68e31cdcde17b0bb6871fe8
Tamir Kfir <tamir.kk@gmail.com>
2019-03-10 00:45:34 +0200

    Change port to be taken from env variables

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const auth = require('./lib/auth');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const checkUser = async (req, res, next) => {
     const userToken = req.headers['x-access-token'];
