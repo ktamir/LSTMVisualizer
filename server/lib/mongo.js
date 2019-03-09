e80dff1d67eb2e620f079c373e04813a2710a92d
Tamir Kfir <tamir.kk@gmail.com>
2019-03-10 00:16:17 +0200

    Configure mongo according to heroku

--- server/lib/mongo.js
+++ server/lib/mongo.js
@@ -1,8 +1,6 @@
-const connectionString = 'mongodb+srv://admin:LSTMAdmin@cluster0-jg7yv.mongodb.net/test?retryWrites=true';
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/lstm";
 const mongoose = require('mongoose');
 
 module.exports.init = () => {
     mongoose.connect(connectionString);
 };
-
-
