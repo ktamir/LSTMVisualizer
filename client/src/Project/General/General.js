263eb96ee82454c36252b6f2cf6d79f447daa478
Tamir Kfir <tamir.kk@gmail.com>
2019-03-23 20:44:47 +0200

    Added general + insights content

--- client/src/Project/General/General.js
+++ client/src/Project/General/General.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {observer, inject} from "mobx-react";
 import axios from "axios";
 import {API_URL} from "../../consts";
+import {Card, Spin} from "antd";
 import './General.scss';
 
 class General extends Component {
@@ -19,17 +20,89 @@ class General extends Component {
         if (this.props.authStore.userToken) {
             const response = await axios.get(API_URL + "/projects/" + this.props.projectId,
                 {headers: {"x-access-token": this.props.authStore.userToken}});
-            this.setState({data: response.data})
+            const general = response.data[0].general_info;
+            this.setState({data: response.data, general});
+            console.log(general);
         }
     };
 
     render() {
         console.log("token: " + this.props.authStore.userToken);
-        console.log(this.state.data);
 
-        return <div className="general-data">
-            {JSON.stringify(this.state.data)}
-        </div>
+        return (
+            <div>
+                {this.state.data ? <div className="general-data">
+                    <div className="row">
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Bidrectional State</h3>
+                                <h1>{this.state.general.bidirectional_state.toString()}</h1>
+                            </div>
+                        </Card>
+
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Dropout</h3>
+                                <h1>{this.state.general.dropout}</h1>
+                            </div>
+                        </Card>
+
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Input Size</h3>
+                                <h1>{this.state.general.input_size}</h1>
+                            </div>
+                        </Card>
+
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Hidden Size</h3>
+                                <h1>{this.state.general.hidden_size}</h1>
+                            </div>
+                        </Card>
+                    </div>
+                    <div className="row">
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Number of Layers</h3>
+                                <h1>{this.state.general.number_of_layers}</h1>
+                            </div>
+                        </Card>
+
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Bias State</h3>
+                                <h1>{this.state.general.bias_state.toString()}</h1>
+                            </div>
+                        </Card>
+
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Batch First</h3>
+                                <h1>{this.state.general.batch_first.toString()}</h1>
+                            </div>
+                        </Card>
+
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Has Hidden Output Layer</h3>
+                                <h1>{this.state.general.has_hidden_output_layer.toString()}</h1>
+                            </div>
+                        </Card>
+
+                    </div>
+
+                    <div className="row">
+                        <Card className="text-content">
+                            <div className="insight-content">
+                                <h3>Hidden Output Dropout Layer</h3>
+                                <h1>{this.state.general.hidden_output_dropout_layer}</h1>
+                            </div>
+                        </Card>
+                    </div>
+                </div> :
+                <Spin/>}
+            </div>)
     }
 }
 
--- client/src/Project/Insights/Insights.js
+++ client/src/Project/Insights/Insights.js
@@ -3,12 +3,14 @@ import {observer, inject} from "mobx-react";
 import axios from "axios";
 import {API_URL} from "../../consts";
 import './Insights.scss';
+import {CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
+import {Spin, Card} from "antd";
 
 class Insights extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {lossData: []};
     }
 
     componentDidMount = async () => {
@@ -19,17 +21,70 @@ class Insights extends Component {
         if (this.props.authStore.userToken) {
             const response = await axios.get(API_URL + "/projects/" + this.props.projectId + "/insights",
                 {headers: {"x-access-token": this.props.authStore.userToken}});
-            this.setState({data: response.data})
+            if (response.data.length > 0) {
+                const {insights} = response.data[0];
+                this.setState({
+                    data: response.data, lossData: insights.loss_values,
+                    optimizer: insights.optimizer, lossFunction: insights.loss_function,
+                    accuracy: insights.accuracy
+                });
+            }
         }
     };
 
     render() {
         console.log("token: " + this.props.authStore.userToken);
+        console.log(this.state.lossData);
+
+        const lossGraphData = this.state.lossData.map((loss_value, index) => ({
+            loss: loss_value,
+            iteration: `#${index + 1}`
+        }));
+
         console.log(this.state.data);
 
-        return <div className="general-data">
-            {JSON.stringify(this.state.data)}
-        </div>
+        return (
+            <div>
+                <div className="row">
+                    <Card>
+                        <div className="insight-content">
+                            {this.state.data ?
+                                <LineChart width={600} height={300} data={lossGraphData}
+                                           margin={{top: 5, right: 20, bottom: 5, left: 0}}>
+                                    <Line type="monotone" dataKey="loss" stroke="#8884d8"/>
+                                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+                                    <XAxis dataKey="iteration"/>
+                                    <YAxis/>
+                                    <Tooltip/>
+                                </LineChart>
+                                : <Spin/>}
+                        </div>
+                    </Card>
+
+                    <Card className="text-content">
+                        <div className="insight-content">
+                            <h3>Optimizer</h3>
+                            <h1>{this.state.optimizer}</h1>
+                        </div>
+                    </Card>
+
+                    <Card className="text-content">
+                        <div className="insight-content">
+                            <h3>Loss Function</h3>
+                            <h1>{this.state.lossFunction}</h1>
+                        </div>
+                    </Card>
+                </div>
+
+                <div className="row">
+                    <Card className="text-content" style={{height: 300}}>
+                        <div className="insight-content">
+                            <h3>Accuracy</h3>
+                            <h1>{this.state.accuracy}</h1>
+                        </div>
+                    </Card>
+                </div>
+            </div>)
     }
 }
 
